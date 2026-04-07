aa90b9d51fb8bb7cee238a81dbdacae3fd612d64
mawki <johncarlo.camara1@gmail.com>
2026-04-08 00:01:05 +0800

    feat: improve seo (#4)
    * feat: add sitemap
    
    * feat: add photos on posts
    
    * feat: improve seo
    
    * fix: remove hardcoded line num
    
    * fix: use utc for midnight shifts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com", // TODO
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
