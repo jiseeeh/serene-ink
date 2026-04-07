692ded2e97aa31a3ba4b0bec8632e7b0af9672f4
jiseeeh <johncarlo.camara1@gmail.com>
2026-04-07 22:05:18 +0800

    feat: add photos on posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,11 +4,12 @@ import { z } from "astro/zod";
 
 const post = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/posts" }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     date: z.string(),
     frontmatter: z.string(),
     tags: z.array(z.string()),
+    image: image().optional(),
   }),
 });
 
